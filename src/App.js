2a9047a2f41631bea5347aae82c8145a2f3bf090
Daniel Rosenthal <daniel.b.rosenthal@gmail.com>
2023-06-12 00:16:06 -0400

    video starts on button click

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import WebcamCapture from "./components/WebcamCapture";
 
 function App() {
+  const [webcamStarted, setWebcamStarted] = useState(false);
+
+  const handleStartWebcam = () => {
+    setWebcamStarted(true);
+  }
   return (
     <Fragment>
-      <WebcamCapture></WebcamCapture>
+      {webcamStarted ? (
+        <WebcamCapture></WebcamCapture>
+      ) : <button onClick={handleStartWebcam}>Start Webcam Capture</button>}
+      
     </Fragment>
   );
 }
---
+++ src/components/EndStream.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+const EndStream = (props) => {
+    return (
+        <Fragment>
+
+        </Fragment>
+    )
+}
+
+export default EndStream;
