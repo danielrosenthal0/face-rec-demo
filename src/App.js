83005e6c94d1bfea1f012a37d2f7c6ec1d5019b3
Daniel Rosenthal <daniel.b.rosenthal@gmail.com>
2023-06-15 01:03:49 -0400

    css is good, might be done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import WebcamCapture from "./components/WebcamCapture";
 import styles from './App.module.css';
 import Header from "./components/Header";
+import solo from './assets/solo.png'
 
 function App() {
   const [webcamStarted, setWebcamStarted] = useState(false);
@@ -18,7 +19,10 @@ function App() {
       <Header></Header>
       {webcamStarted ? (
         <WebcamCapture></WebcamCapture>
-      ) : <button onClick={handleStartWebcam}>Start webcam facial recognition</button>}
+      ) : <div className={styles.stuff}>
+        <img className={styles.guy} src={solo}></img>
+        <button onClick={handleStartWebcam}>Start webcam facial recognition</button>
+        </div>}
       {webcamStarted && <button onClick={handleEndWebcam}>Stop</button>}
     </div>
   );
