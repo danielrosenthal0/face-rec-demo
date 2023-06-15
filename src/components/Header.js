0bd05ecca6867c0d888e27177016d18b28d3847f
Daniel Rosenthal <daniel.b.rosenthal@gmail.com>
2023-06-14 23:19:22 -0400

    added guy top right

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import styles from './Header.module.css';
 
 const Header = (props) => {
     return (
-        <div>
+        <div className={styles.content}>
             <h1>Using face-api in React to create live facial recognition with the webcam</h1>
             <p>Click the start button below and accept the permissions to begin the facial recognition.</p>
         </div>
